a9f865b331aebb9ddd286cacc248f19e4f097e12
Nicholas Augustus <na50324@email.vccs.edu>
2024-02-20 13:06:59 -0500

    Add project files.

---
+++ Split Evens-Odds.cpp
@@ -0,0 +1,49 @@
+// Split Evens-Odds.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include "Split Evens-Odds.h"
+#include "unorderedLinkedList.h"
+using namespace std;
+
+
+// change so that we are re-routing the pointers from the original list and that list ends up being empty
+void intLinkedList::splitEvensOddsList(intLinkedList& evensList,
+	intLinkedList& oddsList)
+{
+	nodeType<int>* current = first;
+
+	while (current != nullptr)
+	{
+		nodeType<int>* nextNode = current->link;
+
+		if (current->info % 2 == 0)
+		{
+			evensList.insertLast(current->info);
+		}
+		else
+		{
+			oddsList.insertLast(current->info);
+		}
+
+		delete current;
+		current = nextNode;
+	}
+	first = nullptr;
+}
+
+int main()
+{
+	intLinkedList evens;
+	intLinkedList odds;
+	intLinkedList test;
+	test.insertLast(22);
+	test.insertLast(55);
+	test.insertLast(13);
+	test.insertLast(829);
+	test.insertLast(46);
+	test.splitEvensOddsList(evens, odds);
+	cout << "evens list" << endl;
+	evens.print(); // do not use print function, use iterator
+	cout << "\nodds list" << endl;
+	odds.print();
+}
