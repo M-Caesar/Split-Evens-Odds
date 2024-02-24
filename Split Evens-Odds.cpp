81f6d61299723208be38a83bacf6f5e1911d6ce4
NICHOLAS AUGUSTUS <nicholasaugustus1@gmail.com>
2024-02-23 23:03:18 -0500

    Changes made

--- Split Evens-Odds.cpp
+++ Split Evens-Odds.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include "Split Evens-Odds.h"
-#include "unorderedLinkedList.h"
+//#include "unorderedLinkedList.h"
 using namespace std;
 
 
@@ -10,8 +10,8 @@ using namespace std;
 void intLinkedList::splitEvensOddsList(intLinkedList& evensList,
 	intLinkedList& oddsList)
 {
-	nodeType<int>* current = first;
-
+	nodeType<int>* current = this->first;
+	/*
 	//change the if and else statements so that they empty the original list as well
 	while (current != nullptr)
 	{
@@ -30,22 +30,81 @@ void intLinkedList::splitEvensOddsList(intLinkedList& evensList,
 		current = nextNode;
 	}
 	first = nullptr;
+	*/
+	while (current != nullptr)
+	{
+		if (current->info %2 == 0)
+		{
+			if (evensList.count == 0)
+			{
+				evensList.first = current;
+			}
+			else
+				evensList.last->link = current;
+
+			evensList.last = current;
+			evensList.count++;
+			current = current->link;
+			evensList.last->link = nullptr;
+		}
+		else
+		{
+			if (oddsList.count == 0)
+			{
+				oddsList.first = current;
+			}
+			else
+				oddsList.last->link = current;
+
+			oddsList.last = current;
+			oddsList.count++;
+			current = current->link;
+			oddsList.last->link = nullptr;
+		}
+	}
+
+	this->first = nullptr;
+	this->last = nullptr;
+	this->count = 0;
 }
-//I am editing this from my phone
 int main()
 {
 	intLinkedList evens;
 	intLinkedList odds;
 	intLinkedList test;
+	/*
+	test.insertLast(24);
 	test.insertLast(22);
 	test.insertLast(55);
 	test.insertLast(77);
 	test.insertLast(13);
 	test.insertLast(829);
 	test.insertLast(46);
+	*/
+	int lister[18] = { 34, 62, 21, 10, 15, 90, 66, 53, 7, 120, 88, 36, 90, 11, 17, 24, 10, -99 };
+	for (int i = 0; i < 18; i++)
+	{
+		test.insertLast(lister[i]);
+	}
 	test.splitEvensOddsList(evens, odds);
+	/*
 	cout << "evens list" << endl;
 	evens.print(); // do not use print function, use iterator
 	cout << "\nodds list" << endl;
 	odds.print();
+	cout << "\noriginal list" << endl;
+	test.print();
+	*/
+	cout << "----------Linked List iterators-----------" << endl << "evens: ";
+	for (auto it = evens.begin(); it != evens.end(); ++it)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+	cout << "odds: ";
+	for (auto it = odds.begin(); it != odds.end(); ++it)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
 }
