abf60bbe5171742f71de339ebf6e20fab5342a7e
NICHOLAS AUGUSTUS <nicholasaugustus1@gmail.com>
2024-02-26 19:12:57 -0500

    Comments adjsuted

--- Split Evens-Odds.cpp
+++ Split Evens-Odds.cpp
@@ -11,26 +11,6 @@ void intLinkedList::splitEvensOddsList(intLinkedList& evensList,
 	intLinkedList& oddsList)
 {
 	nodeType<int>* current = this->first;
-	/*
-	//change the if and else statements so that they empty the original list as well
-	while (current != nullptr)
-	{
-		nodeType<int>* nextNode = current->link;
-
-		if (current->info % 2 == 0)
-		{
-			evensList.insertLast(current->info);
-		}
-		else
-		{
-			oddsList.insertLast(current->info);
-		}
-
-		delete current;
-		current = nextNode;
-	}
-	first = nullptr;
-	*/
 	while (current != nullptr)
 	{
 		if (current->info %2 == 0)
@@ -72,29 +52,12 @@ int main()
 	intLinkedList evens;
 	intLinkedList odds;
 	intLinkedList test;
-	/*
-	test.insertLast(24);
-	test.insertLast(22);
-	test.insertLast(55);
-	test.insertLast(77);
-	test.insertLast(13);
-	test.insertLast(829);
-	test.insertLast(46);
-	*/
 	int lister[18] = { 34, 62, 21, 10, 15, 90, 66, 53, 7, 120, 88, 36, 90, 11, 17, 24, 10, -99 };
 	for (int i = 0; i < 18; i++)
 	{
 		test.insertLast(lister[i]);
 	}
 	test.splitEvensOddsList(evens, odds);
-	/*
-	cout << "evens list" << endl;
-	evens.print(); // do not use print function, use iterator
-	cout << "\nodds list" << endl;
-	odds.print();
-	cout << "\noriginal list" << endl;
-	test.print();
-	*/
 	cout << "----------Linked List iterators-----------" << endl << "evens: ";
 	for (auto it = evens.begin(); it != evens.end(); ++it)
 	{
